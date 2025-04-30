152d5579110d156a874d24101485ff5308c88a66
Akash <akash8979thakur@gmail.com>
2025-04-30 20:54:22 +0530

    add Home App

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def blogHome(request):
+    return HttpResponse('this is blogHome .  ')
+
+
+
+def blogPost(request, slug):
+    return HttpResponse(f'this is blogPost:{slug} ')    
---
+++ home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse('this is home')
+
+def about(request):
+    return HttpResponse('this is about')
+
+def contact(request):
+    return HttpResponse('this is contact')
+
+
+
