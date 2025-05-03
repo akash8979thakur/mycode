2ef9af9d13f77219fa76f8911732e9bf2c979886
Akash <akash8979thakur@gmail.com>
2025-05-03 12:50:33 +0530

    Edit home ,about ,contact & blog

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def blogHome(request):
-    return HttpResponse('this is blogHome .  ')
+    return render(request,'blog/blogHome.html')
 
 
 
 def blogPost(request, slug):
-    return HttpResponse(f'this is blogPost:{slug} ')    
+    return render(request,'blog/blogPost.html')  
--- home/views.py
+++ home/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('this is home')
+    return render(request,'home/home.html')
 
 def about(request):
-    return HttpResponse('this is about')
+    return render(request,'home/about.html')
 
 def contact(request):
-    return HttpResponse('this is contact')
+    return render(request,'home/contact.html')
 
 
 
